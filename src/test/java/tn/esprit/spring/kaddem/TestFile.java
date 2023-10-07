38c6f7db2badc3a0ad009135171566fb8f4f3702
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-10-07 23:49:41 +0200

    Test Update

--- src/test/java/tn/esprit/spring/kaddem/TestFile.java
+++ src/test/java/tn/esprit/spring/kaddem/TestFile.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @Slf4j
-public class UniversiteServiceImplTest {
+public class TestFile {
 
     @InjectMocks
     private UniversiteServiceImpl universiteService;
