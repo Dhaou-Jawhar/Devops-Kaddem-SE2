c482d887927b9f1caf6519fa0245f1fa007ff312
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-10-11 01:08:48 +0200

    test trigger

--- src/test/java/tn/esprit/spring/kaddem/UniversiteServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/UniversiteServiceImplTest.java
@@ -51,13 +51,13 @@ public class UniversiteServiceImplTest {
 
         List<Universite> result = universiteService.retrieveAllUniversites();
 
-        if (result.size() == 3) {
+        if (result.size() == 2) {
             System.err.println("--------------[Test : Find All Universite Method]-------------------\n Test Passed: Result size is 2 as expected. \n -------------------------------------------------------------");
         } else {
             System.err.println("--------------[Test : Find All Universite Method]-------------------\n Test Failed: Expected result size 2, but got " + result.size() + "\n -------------------------------------------------------------");
         }
         //
-        assertEquals(3, result.size());
+        assertEquals(2, result.size());
     }
 
     @Test
