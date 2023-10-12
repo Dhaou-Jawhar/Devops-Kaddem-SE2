a1a5758ee0974bd52b794d63654f87c265d933f5
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-10-12 23:39:08 +0200

    add test file

---
+++ src/test/java/tn/esprit/spring/kaddem/services/ContratServiceImplTest.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContratServiceImplTest {
+
+    @Test
+    void addContrat() {
+
+    }
+}
