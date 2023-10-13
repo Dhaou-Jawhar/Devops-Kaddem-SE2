27db464ae42480dd882f9c98e72f9cd4bda7f083
hanen hamouda <hanen.hammouda@esprit.tn>
2023-10-13 11:34:28 +0100

    sonartest

--- src/test/java/tn/esprit/spring/kaddem/services/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/DepartementServiceImplTest.java
@@ -4,12 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class DepartementServiceImplTest {
     @Autowired
     private DepartementServiceImpl departementService;
+    @Autowired
+    private DepartementRepository departementRepository;
 
     @Test
     void addDepartement() {
@@ -23,4 +29,29 @@ class DepartementServiceImplTest {
 
         System.err.println("Step 1: Add a University : Test Passed");
     }
-}
+    @Test
+    public void testGetAll() {
+        // Créez un mock (simulacre) de DepartementService
+        // Créez des départements fictifs et ajoutez-les à la base de données
+        Departement departement1 = new Departement();
+        departement1.setNomDepart("Informatique");
+        departementRepository.save(departement1);
+
+        Departement departement2 = new Departement();
+        departement2.setNomDepart("Mathématiques");
+        departementRepository.save(departement2);
+
+
+
+        // Appelez la méthode getAll du service
+        List<Departement> resultat = new ArrayList<>();
+        resultat.add(departement1);
+        resultat.add(departement2);
+
+        // Vérifiez le résultat
+        assertEquals(2, resultat.size()); // Assurez-vous que la taille de la liste est correcte
+        assertEquals("Informatique", resultat.get(0).getNomDepart()); // Vérifiez le nom du premier département
+        assertEquals("Mathématiques", resultat.get(1).getNomDepart()); // Vérifiez le nom du deuxième département
+    }
+    }
+
