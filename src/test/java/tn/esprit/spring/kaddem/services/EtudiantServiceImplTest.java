e60c7cf8817670bc3ed6bf20f886c7b527545fa0
Hsm1991 <119038993+Hsm1991@users.noreply.github.com>
2023-10-18 14:23:45 +0100

    Update EtudiantServiceImplTest.java

--- src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/services/EtudiantServiceImplTest.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-
 @Slf4j
 @RunWith(MockitoJUnitRunner.class)
 class EtudiantServiceImplTest {
