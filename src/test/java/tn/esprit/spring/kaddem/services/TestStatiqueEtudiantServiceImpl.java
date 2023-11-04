672496703889613eb5e10b645816d98dd2f12275
houcem1307 <houssemeddine.mhedhbi@esprit.tn>
2023-11-04 18:52:41 +0100

    test

--- src/test/java/tn/esprit/spring/kaddem/services/TestStatiqueEtudiantServiceImpl.java
+++ src/test/java/tn/esprit/spring/kaddem/services/TestStatiqueEtudiantServiceImpl.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.*;
 @Slf4j
 @RunWith(MockitoJUnitRunner.class)
 class TestStatiqueEtudiantServiceImpl {
+//    chbvddy
     @InjectMocks
     private EtudiantServiceImpl etudiantService;
 
