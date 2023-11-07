9fbf5b1852077ca770ff31fd7915bbd408ff7a1c
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-11-07 15:16:07 +0100

    Update Departement.java

--- src/main/java/tn/esprit/spring/kaddem/entities/Departement.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Departement.java
@@ -1,6 +1,7 @@
 package tn.esprit.spring.kaddem.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Departement implements Serializable{
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
