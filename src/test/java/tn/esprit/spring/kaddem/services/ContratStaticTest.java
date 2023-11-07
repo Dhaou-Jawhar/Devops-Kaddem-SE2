44d527b0e6a978fd38b2e2a9226bfff881a1f732
Dhaou-Jawhar <dhaou.jawher@esprit.tn>
2023-11-07 14:30:19 +0100

    Test Added

---
+++ src/test/java/tn/esprit/spring/kaddem/services/ContratStaticTest.java
@@ -0,0 +1,155 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.kaddem.entities.Contrat;
+import tn.esprit.spring.kaddem.entities.Etudiant;
+import tn.esprit.spring.kaddem.entities.Specialite;
+import tn.esprit.spring.kaddem.repositories.ContratRepository;
+import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+public class ContratStaticTest {
+
+    @InjectMocks
+    private ContratServiceImpl contratService;
+
+    @Mock
+    private ContratRepository contratRepository;
+    @Mock
+    private EtudiantRepository etudiantRepository;
+    // You need to create ContratRepository for data access
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllContrats() {
+        // Create a list of sample Contrats
+        List<Contrat> sampleContrats = new ArrayList<>();
+
+        Contrat contrat1 = new Contrat();
+        contrat1.setIdContrat(1);
+        contrat1.setMontantContrat(1000);
+        // Set other properties as needed
+        sampleContrats.add(contrat1);
+
+        Contrat contrat2 = new Contrat();
+        contrat2.setIdContrat(2);
+        contrat2.setMontantContrat(2000);
+        // Set other properties as needed
+        sampleContrats.add(contrat2);
+
+        // Mock the behavior of the contratRepository to return the sampleContrats when retrieveAllContrats is called
+        when(contratRepository.findAll()).thenReturn(sampleContrats);
+
+        // Call the service method
+        List<Contrat> result = contratService.retrieveAllContrats();
+
+        // Verify that the service method returned the expected data
+        assertEquals(sampleContrats, result);
+
+        System.err.println("testRetrieveAllContrats : SUCCESS");
+    }
+
+    @Test
+    public void testAddContrat() {
+        // Create a sample Contrat to add
+        Contrat newContrat = new Contrat();
+        newContrat.setIdContrat(3);
+        newContrat.setDateDebutContrat(new Date()); // You can use java.util.Date for date fields
+        newContrat.setDateFinContrat(new Date());
+        newContrat.setSpecialite(Specialite.IA); // Replace with a valid enum value
+        newContrat.setArchive(false);
+        newContrat.setMontantContrat(5000);
+
+        // Create a sample Etudiant
+        Etudiant etudiant = new Etudiant();
+        etudiant.setNomE("John");
+        newContrat.setEtudiant(etudiant);
+
+        // Mock the behavior of the contratRepository to save the newContrat and return it
+        when(contratRepository.save(newContrat)).thenReturn(newContrat);
+
+        // Call the service method
+        Contrat addedContrat = contratService.addContrat(newContrat);
+
+        // Verify that the service method returned the added contract
+        assertEquals(newContrat, addedContrat);
+
+        // Verify that the save method was called on the contratRepository
+        verify(contratRepository, times(1)).save(newContrat);
+
+        System.err.println("testAddContrat : SUCCESS");
+
+    }
+    @Test
+    public void testUpdateContrat() {
+        // Create a sample Contrat to update
+        Contrat existingContrat = new Contrat();
+        existingContrat.setIdContrat(1);
+        // Set other properties as needed
+
+        // Mock the behavior of the contratRepository to save the updated Contrat and return it
+        when(contratRepository.save(existingContrat)).thenReturn(existingContrat);
+
+        // Call the service method
+        Contrat updatedContrat = contratService.updateContrat(existingContrat);
+
+        // Verify that the service method returned the updated contract
+        assertEquals(existingContrat, updatedContrat);
+
+        // Verify that the save method was called on the contratRepository
+        verify(contratRepository, times(1)).save(existingContrat);
+
+        System.err.println("testUpdateContrat : SUCCESS");
+    }
+    @Test
+    public void testRetrieveContrat() {
+        // Create a sample Contrat with a known ID
+        int id = 1;
+        Contrat sampleContrat = new Contrat();
+        sampleContrat.setIdContrat(id);
+        // Set other properties as needed
+
+        // Mock the behavior of the contratRepository to return the sampleContrat when findById is called
+        when(contratRepository.findById(id)).thenReturn(Optional.of(sampleContrat));
+
+        // Call the service method
+        Contrat result = contratService.retrieveContrat(id);
+
+        // Verify that the service method returned the expected Contrat
+        assertEquals(sampleContrat, result);
+
+        // Verify that the findById method was called on the contratRepository
+        verify(contratRepository, times(1)).findById(id);
+
+        System.err.println("testRetrieveContrat : SUCCESS");
+    }
+    @Test
+    void testDeleteContrat() {
+        Contrat contrat = new Contrat();
+        contrat.setIdContrat(1);
+
+        when(contratRepository.save(contrat)).thenReturn(contrat);
+        contratService.removeContrat(contrat.getIdContrat());
+        List<Contrat> result = contratService.retrieveAllContrats();
+        assertEquals(0, result.size());
+
+        System.err.println("DeleteContratTest : Ok");
+    }
+
+}
---
+++ src/test/java/tn/esprit/spring/kaddem/services/DepartementStaticTest.java
@@ -0,0 +1,125 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DepartementStaticTest {
+
+    //@Autowired
+    @InjectMocks
+    private DepartementServiceImpl departementService;
+    // @Autowired
+    @Mock
+    private DepartementRepository departementRepository;
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetAll() {
+        List<Departement> resultat = new ArrayList<>();
+
+        Departement departement1 = new Departement();
+        departement1.setNomDepart("Esprit Departement");
+        //departementRepository.save(departement1);
+
+        Departement departement2 = new Departement();
+        departement2.setNomDepart("EAmen Departement");
+        // departementRepository.save(departement2);
+
+        resultat.add(departement1);
+        resultat.add(departement2);
+        when(departementRepository.findAll()).thenReturn(resultat);
+        List<Departement> aff =departementService.retrieveAllDepartements();
+        if (resultat.size() == 2) {
+            System.err.println("--------------[Test : Find All Universite Method]-------------------\n Test Passed: Result size is 2 as expected. \n -------------------------------------------------------------");
+        } else {
+            System.err.println("--------------[Test : Find All Universite Method]-------------------\n Test Failed: Expected result size 2, but got " + resultat.size() + "\n -------------------------------------------------------------");
+        }
+        assertEquals(2, resultat.size()); // Assurez-vous que la taille de la liste est correcte
+    }
+
+    @Test
+    void testAddDepartement() {
+        Departement departement = new Departement();
+        departement.setNomDepart("Esprit Departement");
+        when(departementRepository.save(departement)).thenReturn(departement);
+        Departement addedDepartement = departementService.addDepartement(departement);
+        //verify(departementRepository, times(1)).save(departement);
+        assertEquals("Esprit Departement", addedDepartement.getNomDepart());
+        System.out.println(addedDepartement.getNomDepart()+" ajouter");
+    }
+
+    @Test
+    void testRetrieveDepartement() {
+        // Create a sample Departement object
+        Departement dep= new Departement();
+        dep.setIdDepart(1);
+        dep.setNomDepart("Esprit Departement");
+        when(departementRepository.findById(dep.getIdDepart())).thenReturn(Optional.of(dep));
+        Departement retrievedDepartement = departementService.retrieveDepartement(1);
+        verify(departementRepository).findById(1);
+        assertEquals(dep, retrievedDepartement);
+        assertEquals(1, retrievedDepartement.getIdDepart());
+        assertEquals("Esprit Departement", retrievedDepartement.getNomDepart());
+        boolean allAssertionsPassed = true;
+        if (!allAssertionsPassed) {
+            System.err.println("Erreur");
+        } else {
+            System.err.println("Retrieved Departement: " + retrievedDepartement.getNomDepart());
+        }
+    }
+    @Test
+    void testDeleteDepartement() {
+        Departement dep = new Departement();
+        dep.setIdDepart(1);
+        dep.setNomDepart("Esprit Departement");
+        when(departementRepository.findById(dep.getIdDepart())).thenReturn(Optional.of(dep));
+        departementService.deleteDepartement(1);
+        verify(departementRepository).findById(1);
+        verify(departementRepository).delete(dep);
+        if (!departementRepository.existsById(1)) {
+            System.err.println("Departement Deleted Successfully");
+        } else {
+            // Print an error message if the Departement still exists
+            System.err.println("Error: Departement Deletion Failed");
+        }
+    }
+    @Test
+    void testUpdateDepartement() {
+        Departement existingDepartement = new Departement();
+        existingDepartement.setIdDepart(1);
+        existingDepartement.setNomDepart("Esp Departement");
+        when(departementRepository.findById(existingDepartement.getIdDepart())).thenReturn(Optional.of(existingDepartement));
+        when(departementRepository.save(any(Departement.class))).thenAnswer(invocation -> {
+            Departement updatedEntity = invocation.getArgument(0);
+            existingDepartement.setNomDepart(updatedEntity.getNomDepart());
+            return existingDepartement;
+        });
+
+        System.err.println("Before Test Update = " + existingDepartement.getNomDepart());
+        existingDepartement.setNomDepart("Esp1 Departement Name");
+        Departement updatedDepartement = departementService.updateDepartement(existingDepartement);
+        assertEquals("Esp1 Departement Name", updatedDepartement.getNomDepart());
+        verify(departementRepository).save(existingDepartement);
+
+        System.err.println("After Test Update = " + existingDepartement.getNomDepart());
+    }
+
+}
+
